6f4894f7de7ec5fff132ad92996c587076857085
kumi1 <kumi1688@gmail.com>
2020-06-12 09:07:04 +0900

    third

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,49 +1,51 @@
 package com.example.myapplication;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.awareness.Awareness;
 import com.google.android.gms.awareness.SnapshotClient;
-import com.google.android.gms.awareness.fence.AwarenessFence;
-import com.google.android.gms.awareness.fence.DetectedActivityFence;
-import com.google.android.gms.awareness.fence.FenceState;
-import com.google.android.gms.awareness.fence.FenceUpdateRequest;
-import com.google.android.gms.awareness.fence.HeadphoneFence;
-import com.google.android.gms.awareness.snapshot.DetectedActivityResult;
 import com.google.android.gms.awareness.snapshot.HeadphoneStateResponse;
+import com.google.android.gms.awareness.snapshot.LocationResponse;
 import com.google.android.gms.awareness.state.HeadphoneState;
-import com.google.android.gms.common.api.internal.GoogleApiManager;
-import com.google.android.gms.location.ActivityRecognitionResult;
-import com.google.android.gms.location.DetectedActivity;
+import com.google.android.gms.common.api.GoogleApi;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-
-//import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.common.api.GoogleApi;
 import com.google.android.gms.tasks.Task;
 
-import com.example.myapplication.R;
-import java.util.Arrays;
-import java.util.List;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.status)
+    TextView titleTextView;
+
+
     private static final int RC_SIGN_IN = 9001;
+    private static final int MY_PERMISSION_LOCATION = 1;
 
     private GoogleSignInClient mSignInClient;
+
     private GoogleApi mGoogleApi;
     private SnapshotClient mSnapshotClient;
-    private TextView testView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,14 +58,57 @@ public class MainActivity extends AppCompatActivity {
                         .build();
 
         mSignInClient = GoogleSignIn.getClient(this, options);
-
-        getHeadphoneStateV2(testView);
+        ButterKnife.bind(this);
 
 //        signIn();
     }
 
-    //TODO: Method 1 of handling the task result with onComplete Listener
-    public void getHeadphoneState(final TextView v) {
+    @OnClick(R.id.headphone)
+    public void headphoneClick() {
+//        Toast.makeText(this, "안녕하세요!!!", Toast.LENGTH_LONG).show();
+        getHeadphoneState();
+    }
+
+    @OnClick(R.id.location)
+    public void locationClick(){
+        getLocation();
+    }
+
+    public void checkPermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            ActivityCompat.requestPermissions(
+                    MainActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSION_LOCATION
+            );
+        }
+    }
+
+    public void getLocation() {
+        checkPermission();
+        Awareness.getSnapshotClient(this).getLocation().addOnCompleteListener(new OnCompleteListener<LocationResponse>() {
+            @Override
+            public void onComplete(@NonNull Task<LocationResponse> task) {
+                if (task.isSuccessful()) {
+                    LocationResponse result = task.getResult();
+
+                    assert result != null;
+                    Location loc = result.getLocation();
+                    System.out.println(loc);
+
+                } else {
+                    task.getException().printStackTrace();
+                    titleTextView.setText("위치 상태 가져올 수 없음");
+                }
+            }
+        });
+    }
+
+
+        //TODO: Method 1 of handling the task result with onComplete Listener
+    public void getHeadphoneState() {
         Awareness.getSnapshotClient(this).getHeadphoneState().addOnCompleteListener(new OnCompleteListener<HeadphoneStateResponse>() {
             @Override
             public void onComplete(@NonNull Task<HeadphoneStateResponse> task) {
@@ -72,13 +117,13 @@ public class MainActivity extends AppCompatActivity {
                     int state = result.getHeadphoneState().getState();
 
                     if (state == HeadphoneState.PLUGGED_IN) {
-                        v.append("\n___\nHeadphones plugged in.");
+                        titleTextView.setText("헤드폰 꽂힘");
                     } else if (state == HeadphoneState.UNPLUGGED) {
-                        v.append("\n___\nHeadphones unplugged.");
+                        titleTextView.setText("헤드폰 꽂히지 않음");
                     }
                 } else {
                     task.getException().printStackTrace();
-                    v.append("\n___\nUnable to Get Headphones!");
+                    titleTextView.setText("헤드폰 상태 가져올 수 없음");
                 }
             }
         });
@@ -108,6 +153,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
 
     private void signIn() {
         // Launches the sign in flow, the result is returned in onActivityResult
